5c344cb2395ac0482d2e92ed346982109b564f28
Camille <camille@Mac.lan>
2025-05-16 19:04:16 +0200

    Fix CORS again and again

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(cors({
-  origin: ['https://cine-pocket-frontend.vercel.app'], // ou '*' temporairement
+  origin: '*', // ou '*' temporairement
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // uniquement si tu envoies des cookies / headers auth
 }));
 app.options('*', cors()); // <== très important pour les requêtes preflight
+app.use((req, res, next) => {
+  console.log(`📥 ${req.method} ${req.originalUrl}`);
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
